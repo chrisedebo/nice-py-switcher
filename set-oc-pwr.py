c790bab706e03962730877b981d16d848e57d111
Chris Nicel <chrisedebo@gmail.com>
2018-02-15 20:33:51 +0000

    updated output

--- set-oc-pwr.py
+++ set-oc-pwr.py
@@ -11,23 +11,20 @@ gco="GPUGraphicsClockOffset"
 mtro="GPUMemoryTransferRateOffset"
 
 def powerlimit(watts,gpu_no):
-  print(("settings: Power Limit {} watts").format(watts))
   nvidiasmi="sudo nvidia-smi -i {} -pl {}"
   if type(watts) is int:
     nvsmi=nvidiasmi.format(gpu_no,watts)
     subprocess.call((nvsmi).split(" "))
 
 def nvclocksettings(oc_mhz,oc_tag,gpu_no):
-  os.environ["DISPLAY"]=":0"
-  print(("settings: {} {}mhz").format(oc_tag,oc_mhz))
-  nvsettings=("nvidia-settings -a [gpu:{}]/{}[{}]={}").format(gpu_no,oc_tag,"{}",oc_mhz)
-  print(nvsettings.format("1"))
+  os.environ["DISPLAY"]=(":{}").format(gpu_no)
+  nvsettings=("nvidia-settings -a [gpu:{}]/{}[{}]={}").format(0,oc_tag,"{}",oc_mhz)
+  #print(nvsettings.format("1"))
   subprocess.call((nvsettings.format("2")).split(" "))
   subprocess.call((nvsettings.format("3")).split(" "))
 
 cards=int(subprocess.check_output("nvidia-smi --query-gpu=count --format=csv,noheader,nounits".split(" ")).split("\n")[-2])
 for i in range(0, cards):
-  print(i)   
   #power limit
   powerlimit(power_limit,str(i))
 
@@ -38,5 +35,4 @@ for i in range(0, cards):
   nvclocksettings(mem_oc,mtro,str(i))
 
 
-subprocess.call(("nvidia-settings --assign GPULogoBrightness=0").split(" "))
-
+print(("settings: gpu offset:{}mhz mem offset:{}mhz power limit:{} Watts").format(gpu_oc,mem_oc,power_limit))
--- switcher.py
+++ switcher.py
@@ -5,78 +5,14 @@ import sys
 import os
 import urllib.request
 from datetime import datetime
+from influxdb import InfluxDBClient
 
 #Define Functions
+def gethighprofitalgo(influxurl):
 
-#Get JSON from URL
-def getjson(url):
-    with urllib.request.urlopen(url) as response:
-        return response.read()
+#[Telegraf script] collecting data in influx.
 
-#Get algorithm pay ratios
-def getsmaratios(smaurl):
-    #Get nicehash simple multi algorigthm data
-    sma=json.loads(getjson(smaurl))["result"]["simplemultialgo"]
-    
-    return sma
-
-#Get Exghangerate Fiat/BTC
-def getexrate(exrateurl,fiat):
-    #Get coinbase exchangerate
-    exrates=json.loads(getjson(exrateurl))
-    exrate=float(exrates["data"]["rates"][fiat])
-    if loglevel >= 5:
-        print(("exchange rate:{}").format(exrate))
-
-    return exrate
-
-#Get algorithm profit values
-def getprofits(sma,exrate,algos):
-
-    algoprofit={}
-    for n in range(0,len(sma)):
-        algo=sma[n]
-        ghs=0.0
-        kwh=0.0
-        
-        try:
-            #Get performance and power stats
-            ghs=algos[algo["name"]]["ghs"]
-            kwh=algos[algo["name"]]["kwh"]
-            
-            #Get pay ratio
-            paying=float(algo["paying"])
-            
-            #Calculate earnings, cost and profit
-            incoming=float(ghs*paying*exrate)
-            outgoing=float(kwh*24.0*pwr_cost)
-            profit=incoming-outgoing
-
-            name=algo["name"]
-            algoprofit[name]=profit
-            
-            if loglevel >= 5:
-                rateformat="algo: {}  paying: {}  BTC/GH/Day  in: €{}  out: €{}  net:€{}"
-                print(rateformat.format('{:10}'.format(name), \
-                                        '{:.5f}'.format(round(paying,5)), 
-                                        '{:.5f}'.format(round(incoming,5)), \
-                                        '{:.5f}'.format(round(outgoing,5)), \
-                                        round(profit,5)) \
-                                        )
-            
-            
-        except:
-            #if logenum >=1:
-            #print(algo)
-            pass
-        
-    return algoprofit
-
-
-
-#Get nicehash simple multi algorigthm data
-#TODO 
-#Thread: Check profitability
+#Thread: Check profitability from influx
 # Loop
 #  Check time elapsed vs mintime
 #  If time elapsed < mintime
@@ -97,39 +33,18 @@ def getprofits(sma,exrate,algos):
 #   If not running most profitable
 #    Stop running miner
 #
-#Thread: Monitor
-# Loop
-#  Check temperature (temp limit in config)
-#  Check Fan speed
-#  Heuristic curve for fan speed vs temperature
-#
-#Thread: Statistics collection
-# Loop
-#  Check stats and report to influxdb
-#  profitiability and max other profitability (top algorithm if within min time and switch threshold, 2nd if not)
-#  miner api: hashrate, power, difficulty, gpu usage, memory usage, clock frequencies, temperature, fan speed
-#
-#Thread: Tuning
-# Monitor KHW from miner api
-# Adjust GPU and MEM clocks
-# Try to acheive best performance per watt.
-#
-
 
 #Load Config
 cfgfile=sys.argv[1]
 config=json.loads(open(cfgfile).read())
 
 #Get config values
-fiat=config["fiat"]
 min_prft=config["min_profit"]
-pwr_cost=config["pwr_cost"]
 pmnt_addr=config["pmnt_addr"]
 miner_name=config["miner_name"]
 switch_threshold=config["switch_threshold"]
 algos=config["algorithms"]
-exrateurl=config["exrateurl"]
-smaurl=config["smaurl"]
+influxurl=config["influxurl"]
 
 #Set up logging
 loglevels={"debug":5,"info":4,"warn":3,"error":2,"fatal":1,"off":0}
@@ -137,17 +52,4 @@ loglevel=loglevels[config["loglevel"]]
 logfile=config["logfile"]
 
 
-sma=getsmaratios(smaurl)
-exrate=getexrate(exrateurl,fiat)
-algoprofit=getprofits(sma,exrate,algos)
-
-max_profit=0.0
-max_profit_algo=""
-for algo in algoprofit:
-  profit=(algoprofit[algo])
-  if profit > max_profit:
-    max_profit=profit
-    max_profit_algo=algo
-
-print(("{},{},{}").format(datetime.now(),max_profit_algo,max_profit))
 
